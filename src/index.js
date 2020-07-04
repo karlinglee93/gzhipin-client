a7df31b926a16130fe9bf0b410b5f35e47b01759
lijianig <lijianig@cn.ibm.com>
2020-07-05 00:46:46 +0800

    feat: add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button} from 'antd-mobile'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
-ReactDOM.render(<Button type='primary'>Test</Button>, document.getElementById('root'))
+import Registry from './containers/registry/registry'
+import Login from './containers/login/login'
+import Main from './containers/main/main'
+
+
+ReactDOM.render((
+	<HashRouter>
+		<Switch>
+			<Route path='/registry' component={Registry} />
+			<Route path='/login' component={Login} />
+			<Route component={Main} />
+		</Switch>
+	</HashRouter>
+), document.getElementById('root'))
