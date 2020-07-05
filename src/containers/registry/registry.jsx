ebc95c793e841043561c03992e9f71a51d6f12bc
lijianig <lijianig@cn.ibm.com>
2020-07-05 23:54:13 +0800

    fix(registry): update events
    1. get [name]
    2. push() and replace() in this.props.history

--- src/containers/registry/registry.jsx
+++ src/containers/registry/registry.jsx
@@ -14,7 +14,31 @@ import Logo from '../../components/logo/logo'
 const {Item} = List
 
 export default class Registry extends Component {
+
+	state = {
+		username: '',
+		password: '',
+		password2: '',
+		type: 'dashen'
+	}
+
+	handleRegistry = () => {
+		console.log(this.state)
+	}
+
+	handleChange = (name, val) => {
+		this.setState({
+			[name]: val // 属性名不是name，而是变量name的值。用[]取值
+		})
+	}
+
+	toLogin = () => {
+		this.props.history.replace('/login') // push() and replace()
+	}
+
 	render () {
+		const {type} = this.state
+
 		return (
 			<div>
 				<NavBar mode='dark'>特&nbsp;聘&nbsp;搬&nbsp;砖&nbsp;工</NavBar>
@@ -22,23 +46,23 @@ export default class Registry extends Component {
 				<WingBlank size='lg'>
 					<List>
 						<WhiteSpace size='md' />
-						<InputItem type='text'>用户名</InputItem>
+						<InputItem type='text' onChange={val => {this.handleChange('username', val)}}>用户名</InputItem>
 						<WhiteSpace size='md' />
-						<InputItem type='password'>密&nbsp;&nbsp;&nbsp;码</InputItem>
+						<InputItem type='password' onChange={val => {this.handleChange('password', val)}}>密&nbsp;&nbsp;&nbsp;码</InputItem>
 						<WhiteSpace size='md' />
-						<InputItem type='password' placeholder='请确认密码'>确认密码</InputItem>
+						<InputItem type='password' onChange={val => {this.handleChange('password2', val)}} placeholder='请确认密码'>确认密码</InputItem>
 						<WhiteSpace size='md' />
 						<Item>
 							<span>用户类型</span>
 							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							<Radio>大神</Radio>
+							<Radio checked={type === 'dashen'} onChange={() => {this.handleChange('type', 'dashen')}}>大神</Radio>
 							&nbsp;&nbsp;&nbsp;
-							<Radio>老板</Radio>
+							<Radio checked={type === 'laoban'} onChange={() => {this.handleChange('type', 'laoban')}}>老板</Radio>
 						</Item>
 						<WhiteSpace size='md' />
-						<Button type='primary'>注&nbsp;&nbsp;&nbsp;册</Button>
+						<Button type='primary' onClick={this.handleRegistry}>注&nbsp;&nbsp;&nbsp;册</Button>
 						<WhiteSpace size='md' />
-						<Button>已有账户</Button>
+						<Button onClick={this.toLogin}>已有账户</Button>
 					</List>
 				</WingBlank>
 			</div>
