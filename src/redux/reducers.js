379855dcf211bb1718d99979613fbcdb3c9061e2
lijianig <lijianig@cn.ibm.com>
2020-07-09 23:38:44 +0800

    fix(Redux): create reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,5 +1,7 @@
 import {combineReducers} from 'redux'
 
+import {AUTH_SUCCESS, ERR_MSG} from './action-types'
+
 const initUser = {
 	username: '',
 	type: '',
@@ -9,7 +11,11 @@ const initUser = {
 // 产生user状态的reducer
 const user = (state = initUser, action) => {
 	switch (action.type) {
-	
+		case: AUTH_SUCCESS
+			// 先返回state, 并用action.data将前者覆盖掉
+			return {...state, ...action.data}
+		case: ERR_MSG
+			return {...state, msg: action.data}
 		default:
 			return state
 	}
