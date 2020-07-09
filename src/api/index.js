89734482396720c634a38f91ea4435be41a98872
lijianig <lijianig@cn.ibm.com>
2020-07-09 21:43:05 +0800

    fix(Ajax): update

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+/**
+ * 包含n个接口请求函数的模块
+ * 每个函数的返回值都是Promise对象
+ */
+import ajax from './ajax'
+
+// 注册接口
+export const reqRegister = user => ajax('/register', user, 'POST')
+// 登陆接口
+export const reqLogin = user => ajax('/login', user, 'POST')
