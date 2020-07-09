47f989b431f5847840c1aa64d61760c80ccfa4b4
lijianig <lijianig@cn.ibm.com>
2020-07-09 23:43:19 +0800

    fix

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -11,10 +11,10 @@ const initUser = {
 // 产生user状态的reducer
 const user = (state = initUser, action) => {
 	switch (action.type) {
-		case: AUTH_SUCCESS
+		case AUTH_SUCCESS:
 			// 先返回state, 并用action.data将前者覆盖掉
 			return {...state, ...action.data}
-		case: ERR_MSG
+		case ERR_MSG:
 			return {...state, msg: action.data}
 		default:
 			return state
