aa15b0ccd8618b99f68dea4e62e38f42c6e0d67e
lijianig <lijianig@cn.ibm.com>
2020-07-11 00:24:16 +0800

    fix(Scss): update

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 
 import logo from './logo.png'
-import './logo.scss'
 
 export default class Logo extends Component {
 	render () {
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Register from './containers/register/register'
 import Login from './containers/login/login'
 import Main from './containers/main/main'
 import store from './redux/store'
+import './assets/css/index.scss'
 
 ReactDOM.render((
 	<Provider store={store}>
