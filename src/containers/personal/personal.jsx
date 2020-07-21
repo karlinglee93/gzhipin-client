d0ef7b9e9bff767d2d6c31938c4c985eb7bba376
lijianig <lijianig@cn.ibm.com>
2020-07-21 15:13:52 +0800

    feat(Personal): static

--- src/containers/personal/personal.jsx
+++ src/containers/personal/personal.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {Result, List, WhiteSpace, Button} from 'antd-mobile'
+
+const Item = List.Item
+const Brief = Item.Brief
 
 export class Personal extends Component {
 	render() {
 		return (
 			<div>
-				Personal
+				<Result 
+					img={<img src={require(`../../assets/images/headers/头像1.png`)} style={{width: 50}} alt='header' />}
+					title='Username'
+					message='Company'
+				/>
+				<List renderHeader={() => '相关信息'}>
+					<Item multipleLine>
+						<Brief>职位: post</Brief>
+						<Brief>简介: info</Brief>
+						<Brief>薪资: salary</Brief>
+					</Item>
+				</List>
+				<WhiteSpace />
+				<Button type='warning'>退出登录</Button>
 			</div>
 		)
 	}
