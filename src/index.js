6cdf1e11e7504b39274652b3ad71177b5644a5a6
lijianig <lijianig@cn.ibm.com>
2020-07-31 00:13:57 +0800

    feat(SocketIO): test

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import Main from './containers/main/main'
 import store from './redux/store'
 import './assets/css/index.scss'
 
+import './test/socketio_test'
+
 ReactDOM.render((
 	<Provider store={store}>
 		<HashRouter>
---
+++ src/test/socketio_test.js
@@ -0,0 +1,11 @@
+import io from 'socket.io-client'
+
+// 连接服务器, 得到代表连接的socket对象
+const socket = io('ws://localhost:4001')
+// 向服务器发送消息
+socket.emit('sendMsg', {name: 'Tom', date: Date.now()})
+console.log('客户端向服务器发送消息: ', {name: 'Tom', date: Date.now()})
+// 绑定监听, 来接收服务器发送的消息
+socket.on('receiveMsg', data => {
+	console.log('客户端接收到消息: ', data)
+})
